dadba52ca344c533b1ab77a184d167a408e016f0
Todd Wolfson <todd@twolfson.com>
2016-03-23 13:55:41 -0500

    Added basic HTML/CSS

--- README.md
+++ README.md
@@ -5,7 +5,38 @@ Web application for https://findwork.co/
 TODO: Add me
 
 ## Documentation
-TODO: Add me
+### Development
+Currently, we don't have any server components. To host the files locally in development, we are using `serve`.
+
+To run a development server locally, run:
+
+```bash
+npm start
+```
+
+The files will be available at <http://localhost:9000/server/views/>
+
+### Building files
+We offer compilation of CSS/JS via 2 means:
+
+**Single run:** Compile our files only once
+
+```bash
+npm run build
+```
+
+**Continuous:** Compile our files and re-compile when they change
+
+```bash
+npm run develop
+```
+
+### Automated refresh
+We integrate with LiveReload by starting a LiveReload server when `npm run develop` is running.
+
+As a result, if you enable/install a LiveReload browser extension, then it will automatically reload the page when files change.
+
+http://livereload.com/extensions/
 
 ## Copyright
 All rights reserved, Shoulders of Titans LLC
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+// Load in our dependencies
+var gulp = require('gulp');
+var gulpCsso = require('gulp-csso');
+var gulpLivereload = require('gulp-livereload');
+var gulpSass = require('gulp-sass');
+var gulpSizereport = require('gulp-sizereport');
+var rimraf = require('rimraf');
+
+// Set up our configuration
+var config = {
+  allowFailures: false,
+  minifyAssets: true
+};
+
+// Define our build tasks
+gulp.task('build-clean', function clean (done) {
+  // Remove all compiled files in `dist/`
+  rimraf(__dirname + '/dist/', done);
+});
+
+gulp.task('build-css', function buildCss () {
+  // Generate a stream that compiles SCSS to CSS
+  // DEV: We return the pipe'd stream so gulp knows when we exit
+  var cssStream = gulp.src('public/css/index.scss')
+    .pipe(gulpSass({
+      style: 'nested'
+    }));
+
+  // If we are allowing failures, then log them
+  if (config.allowFailures) {
+    cssStream.on('error', console.error);
+  }
+
+  // If we are minifying assets, then minify them
+  if (config.minifyAssets) {
+    cssStream = cssStream
+      .pipe(gulpCsso())
+      .pipe(gulpSizereport({gzip: true}));
+  }
+
+  // Output our CSS and notify LiveReload
+  return cssStream
+    .pipe(gulp.dest('dist/css'))
+    .pipe(gulpLivereload());
+});
+
+gulp.task('build', ['build-css']);
+
+// Define our development tasks
+gulp.task('livereload-update', function livereloadUpdate () {
+  gulpLivereload.reload();
+});
+
+// DEV: `['build']` requires that our build task runs once
+gulp.task('develop', ['build'], function develop () {
+  // Set up our tasks to allow failures
+  config.allowFailures = true;
+  config.minifyAssets = false;
+
+  // Start a livereload server
+  gulpLivereload.listen();
+
+  // When one of our src files changes, re-run its corresponding task
+  gulp.watch('public/css/**/*.scss', ['build-css']);
+  gulp.watch('server/**/*', ['livereload-update']);
+});
