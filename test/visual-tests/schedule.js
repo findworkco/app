5533804d66de0c13ba9f357d1d746c268dd7409d
Todd Wolfson <todd@twolfson.com>
2016-04-06 19:12:53 -0500

    Cleaned up one-off TODOs
    We are trying to get to a better state to manage our code. That means taking care of some placeholder TODOs. In this PR:
    
    - Renamed `nav` views to `schedule` for clarity
    - Removed old TODOs
    - Added CSRF input placeholder
    - Added a11y headings and roles
    - Added dynamic title requirement

--- test/visual-tests/schedule.js
+++ test/visual-tests/schedule.js
@@ -3,9 +3,9 @@ var gemini = require('gemini');
 var geminiUtils = require('./utils/gemini');
 
 // Define our visual tests
-gemini.suite('nav', function (suite) {
+gemini.suite('schedule', function (suite) {
   // DEV: We include nav to make sure we have no links selected
-  suite.setUrl('/nav.jade')
+  suite.setUrl('/schedule.jade')
     .setCaptureElements('body')
     .capture('default-large', geminiUtils.resizeLarge)
     .capture('default-medium', geminiUtils.resizeMedium)
