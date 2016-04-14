c32a6e0eacb3d2abccc5a0b8b7618e01fd03e599
Todd Wolfson <todd@twolfson.com>
2016-04-14 18:42:50 -0500

    Repaired mobile datepicker (#29)
    We had 2 datepickers opening on mobile as well as Android Chrome being substantially slow. In this PR:
    
    - Added Modernizr to skip fallback datepicker binding
    - Added `min/max` to repair Android Chrome performance issues
    - Added `datepicker` mixin for code reuse

--- public/js/index.js
+++ public/js/index.js
@@ -2,24 +2,26 @@
 // TODO: Reduce size of jQuery if it gets unwieldy (e.g. remove AJAX)
 var $ = require('jquery');
 void require('bootstrap-datepicker');
+var Modernizr = require('browsernizr');
+void require('browsernizr/test/inputtypes');
 
 // When the DOM is ready
 // DEV: If our one-off plugins get unwieldy, relocate them to files that export an `init` function
 $(function handleReady () {
-  // Bind all datepicker inputs
-  // http://eternicode.github.io/bootstrap-datepicker/
-  // https://bootstrap-datepicker.readthedocs.org/en/latest/index.html
-  // TODO: Add component test to verify when we click a new date it changes
-  //   and when we return to the original they are the same (verifies moment + component consistency)
-  // TODO: Figure out how to do binding on mobile (prob need to use Modernizr or some adapter)
-  $('input[type=date]').each(function handleDateInput () {
-    var $el = $(this);
-    $el.datepicker({
-      // Fri Jan 8 2016
-      // TODO: Use same length for short months/days as moment in the picker
-      format: 'D M d, yyyy',
-      todayBtn: 'linked',
-      todayHighlight: true
+  // If we don't have native support for datepicker, then use a fallback
+  if (!Modernizr.inputtypes.date) {
+    // http://eternicode.github.io/bootstrap-datepicker/
+    // https://bootstrap-datepicker.readthedocs.org/en/latest/index.html
+    // TODO: Add component test to verify when we click a new date it changes
+    //   and when we return to the original they are the same (verifies moment + component consistency)
+    $('input[type=date]').each(function handleDateInput () {
+      var $el = $(this);
+      $el.datepicker({
+        // 2016-01-08
+        format: 'yyyy-mm-dd',
+        todayBtn: 'linked',
+        todayHighlight: true
+      });
     });
-  });
+  }
 });
