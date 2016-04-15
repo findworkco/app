e239108e273bb4302c729af8fb55db4e46169ee2
Todd Wolfson <todd@twolfson.com>
2016-04-14 19:15:20 -0500

    Cleaned up more TODOs (#30)
    We are trying to get our TODOs down to a more managable state. In this PR:
    
    - Consolidated redundant TODOs
    - Removed no longer relevant TODOs
    - Completed small one-offs (e.g. relocating grid JS)

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 // Load in our dependencies
-// TODO: Reduce size of jQuery if it gets unwieldy (e.g. remove AJAX)
+// TODO: Reduce size of jQuery as it's quite large (e.g. remove AJAX)
 var $ = require('jquery');
 void require('bootstrap-datepicker');
 var Modernizr = require('browsernizr');
 void require('browsernizr/test/inputtypes');
+
+// TODO: Construct an autosave mechanism for `data-autosave`
 
 // When the DOM is ready
 // DEV: If our one-off plugins get unwieldy, relocate them to files that export an `init` function
@@ -23,5 +25,23 @@ $(function handleReady () {
         todayHighlight: true
       });
     });
+  }
+
+  // If we are on a page with a `?grid` query parameter, then render a grid
+  // TODO: Move to query string (currently breaks serve)
+  if (window.location.hash === '#grid') {
+    // Define our overrides
+    // https://github.com/peol/960gridder/blob/677b61a7/releases/1.3.1/960.gridder.src.js#L42-L61
+    window.gOverride = {
+      gColumns: 12,
+      gWidth: 9,
+      pHeight: 18
+    };
+
+    // Load our grid
+    var scriptEl = document.createElement('script');
+    var headEl = document.querySelector('head');
+    scriptEl.src = 'https://rawgit.com/peol/960gridder/677b61a7/releases/1.3.1/960.gridder.src.js';
+    headEl.appendChild(scriptEl);
   }
 });
