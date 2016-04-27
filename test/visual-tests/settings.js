d22f24f2881422130e78af8f4caf641d9cd1e856
Todd Wolfson <todd@twolfson.com>
2016-04-27 18:27:59 -0500

    Added large screen for settings page (#44)
    We are continuing prototying our pages. In this PR:
    
    - Added large screen for settings page
    - Added visual test
    
    ![firefox](https://cloud.githubusercontent.com/assets/902488/14871164/8eb8590c-0ca5-11e6-8e4e-d6d30d53c463.png)

---
+++ test/visual-tests/settings.js
@@ -0,0 +1,11 @@
+// Load in our dependencies
+var gemini = require('gemini');
+var geminiUtils = require('./utils/gemini');
+
+// Define our visual tests
+gemini.suite('settings', function (suite) {
+  // DEV: We include nav to make sure we have selected the proper link
+  suite.setUrl('/settings.jade')
+    .setCaptureElements('body')
+    .capture('default-large', geminiUtils.resizeLarge);
+});
