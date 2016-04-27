3006000fcae75aaf618c7bacfba071d98b1b0759
Todd Wolfson <todd@twolfson.com>
2016-04-27 18:50:08 -0500

    Added medium and small screens for settings page (#45)
    We have completed another page, this time the settings page. In this PR:
    
    - Added medium and small screens for settings page
    - Added visual tests
    
    ![firefox](https://cloud.githubusercontent.com/assets/902488/14871520/905f67a2-0ca8-11e6-8e07-3cc32a0edb5f.png)
    
    ![firefox](https://cloud.githubusercontent.com/assets/902488/14871522/92cc87e0-0ca8-11e6-90e6-932cd9a4cf05.png)

--- test/visual-tests/settings.js
+++ test/visual-tests/settings.js
@@ -7,5 +7,7 @@ gemini.suite('settings', function (suite) {
   // DEV: We include nav to make sure we have selected the proper link
   suite.setUrl('/settings.jade')
     .setCaptureElements('body')
-    .capture('default-large', geminiUtils.resizeLarge);
+    .capture('default-large', geminiUtils.resizeLarge)
+    .capture('default-medium', geminiUtils.resizeMedium)
+    .capture('default-small', geminiUtils.resizeSmall);
 });
