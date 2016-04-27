437ccb8f077186f6224a11903ef71c20dedc048a
Todd Wolfson <todd@twolfson.com>
2016-04-27 17:04:11 -0500

    Added medium and small screens for interview edit (#43)
    We have completed the remaining screens for our interview edit page. In this PR:
    
    - Added medium and small screens for interview edit
    - Added visual tests
    
    ![firefox](https://cloud.githubusercontent.com/assets/902488/14869583/eccb6662-0c99-11e6-8bab-ec49cb8a1192.png)
    
    ![firefox](https://cloud.githubusercontent.com/assets/902488/14869586/efe5079a-0c99-11e6-8645-eaf8bb35d881.png)

--- test/visual-tests/interview-edit-show.js
+++ test/visual-tests/interview-edit-show.js
@@ -7,5 +7,7 @@ gemini.suite('interview-edit-show', function (suite) {
   // DEV: We include nav to make sure we have selected the proper link
   suite.setUrl('/interview-edit-show.jade')
     .setCaptureElements('body')
-    .capture('default-large', geminiUtils.resizeLarge);
+    .capture('default-large', geminiUtils.resizeLarge)
+    .capture('default-medium', geminiUtils.resizeMedium)
+    .capture('default-small', geminiUtils.resizeSmall);
 });
