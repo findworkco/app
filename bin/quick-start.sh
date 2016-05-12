a94395cc9d84f5562538b5818ebf45452a6eb55c
Todd Wolfson <todd@twolfson.com>
2016-05-12 13:54:23 -0500

    Future-proofed quick start script (#56)
    We had a thought about further improving our quick start script. In this PR:
    
    - Moved to multi-process support for quick start script
        - This includes signal supports for SIGINT and SIGTERM
        - This is achieved by dual-using `quick-start.sh` to run as a subshell script as well as an in-session script

--- bin/quick-start.sh
+++ bin/quick-start.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
-# DEV: We skip `set -e` as we are executing this via `.`
-# Navigate to the repo directory
-cd /vagrant/app
+# If we aren't in `/vagrant/app`
+if test "$PWD" != "/vagrant/app"; then
+  # Navigate to `/vagrant/app` and re-execute ourself in a subshell context
+  # DEV: We skip `set -e` since this is typicall executed via `.`
+  # DEV: We use a subshell to allow for forking (`&`), `wait`, and signals to work properly (otherwise, we exit `ssh`)
+  cd /vagrant/app
+  bin/quick-start.sh
+# Otherwise, start a development server
+# DEV: Eventually we should launch other processes concurrently (e.g. `kue`)
+else
+  # Exit on first error
+  set -e
 
-# Run our development script
-npm run start-develop
+  # Set up handlers for signals from user
+  # http://unix.stackexchange.com/a/146770
+  handle_sigint () {
+    set +e
+    kill -s SIGINT "$server_pid"
+  }
+  handle_sigterm () {
+    set +e
+    kill -s SIGTERM "$server_pid"
+  }
+  trap handle_sigint SIGINT
+  trap handle_sigterm SIGTERM
+
+  # Run our development server
+  npm run start-develop &
+  server_pid="$!"
+
+  # Wait for all child processes to exit before exiting ourself
+  # DEV: This guarantees our `trap` handlers complete
+  wait
+fi
+
