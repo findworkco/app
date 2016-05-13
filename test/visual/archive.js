d4df004c55b7a2bc7ecfc6437216b34d144b62ec
Todd Wolfson <todd@twolfson.com>
2016-05-13 12:29:58 -0500

    Added `/archive` medium and small screens (#60)
    We are continuing with prototyping. In this PR:
    
    - Added remaining medium and small screens for `/archive`
    - Added visual tests
    
    ![firefox](https://cloud.githubusercontent.com/assets/902488/15234098/a2d66972-1873-11e6-8144-36ff72890cdb.png)
    
    ![firefox](https://cloud.githubusercontent.com/assets/902488/15234100/a4d9e8fc-1873-11e6-8be8-a8d8598d31fb.png)

--- test/visual/archive.js
+++ test/visual/archive.js
@@ -7,5 +7,7 @@ gemini.suite('archive', function (suite) {
   // DEV: We include nav to make sure we show archived applications and have no links selected
   suite.setUrl('/archive')
     .setCaptureElements('body')
-    .capture('default-large', geminiUtils.resizeLarge);
+    .capture('default-large', geminiUtils.resizeLarge)
+    .capture('default-medium', geminiUtils.resizeMedium)
+    .capture('default-small', geminiUtils.resizeSmall);
 });
