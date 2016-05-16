dc893daade7a85582f74b7d99dfd28551ec395db
Todd Wolfson <todd@twolfson.com>
2016-05-16 16:34:03 -0500

    Stabilized dynamic values in visual tests (#65)
    In application add, we had dynamic values for reminders being set. This was breaking our visual tests. In this PR:
    
    - Added server based tests to validate values were as expected
    - Updated visual tests to normalize dynamic values to expected ones

--- test/visual/application-add-show.js
+++ test/visual/application-add-show.js
@@ -7,6 +7,13 @@ gemini.suite('application-add-show', function (suite) {
   // DEV: We include nav to make sure we have no links selected
   suite.setUrl('/add-application')
     .setCaptureElements('body')
+    .before(function normalizeRelativeValues (actions, find) {
+      // Reset date/time inputs to consistent time
+      actions.executeJS(function handleExecuteJS (window) {
+        window.document.querySelector('[name=application_reminder_date]').value = '2016-05-20';
+        window.document.querySelector('[name=application_reminder_time]').value = '7:00PM';
+      });
+    })
     .capture('default-large', geminiUtils.resizeLarge)
     .capture('default-medium', geminiUtils.resizeMedium)
     .capture('default-small', geminiUtils.resizeSmall);
