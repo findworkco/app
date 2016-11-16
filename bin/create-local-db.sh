6c4952bc0e9060288127b35c2bf26393358afab6
Todd Wolfson <todd@twolfson.com>
2016-11-15 20:32:48 -0800

    Fixed visual tests (#144)
    Our visual tests broke due to not keeping `/_dev/setup` up to date with our new candidate login system. In this PR:
    
    - Updated `/_dev/setup` to use ids
    - Added development users to `create-local-db.sh` for visual tests

--- bin/create-local-db.sh
+++ bin/create-local-db.sh
@@ -22,6 +22,41 @@ sudo su postgres --shell /bin/bash --command "createdb \"$db_name\""
 # Run our migrations
 npm run migrate-latest
 
+# If we are in development, create development users
+if test "$ENV" = "development"; then
+  node --eval "
+  // Load in our dependencies
+  var async = require('async');
+  void require('./server/index.js');
+  var AuditLog = require('./server/models/audit-log');
+  var Candidate = require('./server/models/candidate');
+
+  // In parallel, create our development users (used for screenshots)
+  async.parallel([
+    function createScreenshotCandidate (cb) {
+      Candidate.create({
+        id: '00000000-0000-0000-0000-000000000000',
+        email: 'todd@findwork.co'
+      }, {_sourceType: AuditLog.SOURCE_SERVER}).asCallback(cb);
+    },
+    function createMockupCandidate (cb) {
+      Candidate.create({
+        id: '00000000-0000-0000-0000-000000000001',
+        email: 'dev-user@findwork.test'
+      }, {_sourceType: AuditLog.SOURCE_SERVER}).asCallback(cb);
+    }
+  ], function handleError (err) {
+    // If there was an error, throw it
+    if (err) {
+      throw err;
+    }
+
+    // Otherwise, exit cleanly
+    process.exit(0);
+  });
+  "
+fi
+
 # Notify user of success
 echo "Database \"$db_name\" successfully created" 1>&2
 
--- server/controllers/development.js
+++ server/controllers/development.js
@@ -22,10 +22,10 @@ app.get('/_dev/setup', function devSetupShow (req, res, next) {
   // Override current session with mock user info
   // Example usage: `/_dev/schedule?logged_in=true`
   if (req.query.get('logged_in') === 'true') {
-    req.session.passport = {user: 'dev-user@findwork.test'};
+    req.session.passport = {user: '00000000-0000-0000-0000-000000000001'};
   }
   if (req.query.get('screenshot') === 'true') {
-    req.session.passport = {user: 'todd@findwork.co'};
+    req.session.passport = {user: '00000000-0000-0000-0000-000000000000'};
   }
 
   // If there is a request for clean CSS, set it up
