f661671344bdc4d8cb8648995d5bafda1ab4028a
Todd Wolfson <todd@twolfson.com>
2016-12-28 17:58:33 -0800

    Moved to dynamic data mocks (#185)
    We are about to start changing includes depending on controllers. This PR makes the mock data line up with that. In this PR:
    
    - Moved to dynamic data mocks

--- server/models/application-mock-data.js
+++ server/models/application-mock-data.js
@@ -1,4 +1,5 @@
 // Load in our dependencies
+var assert = require('assert');
 var _ = require('underscore');
 var HttpError = require('http-errors');
 var Application = require('./application');
@@ -16,30 +17,65 @@ genericMockData.applications.forEach(function saveApplicationById (application) 
 // http://docs.sequelizejs.com/en/v3/docs/associations/#creating-with-associations
 var interviewMocks = genericMockData.interviews;
 var reminderMocks = genericMockData.reminders;
-exports._buildApplicationAttrs = function (attrs) {
-  return _.extend({}, attrs, {
-    interviews: _.where(interviewMocks, {application_id: attrs.id}),
-    saved_for_later_reminder: _.findWhere(reminderMocks, {id: attrs.saved_for_later_reminder_id}),
-    waiting_for_response_reminder: _.findWhere(reminderMocks, {id: attrs.waiting_for_response_reminder_id}),
-    received_offer_reminder: _.findWhere(reminderMocks, {id: attrs.received_offer_reminder_id})
-  });
-};
-exports._buildApplicationInclude = function (attrs) {
-  // DEV: We use `_.flatten` and empty arrays to skip non-included data
-  return _.flatten([
-    {model: Interview},
-    attrs.saved_for_later_reminder_id ? {model: Reminder, as: 'saved_for_later_reminder'} : [],
-    attrs.waiting_for_response_reminder_id ? {model: Reminder, as: 'waiting_for_response_reminder'} : [],
-    attrs.received_offer_reminder_id ? {model: Reminder, as: 'received_offer_reminder'} : []
-  ]);
-};
-function buildApplication(attrs) {
-  // Build our application
-  var retVal = Application.build(exports._buildApplicationAttrs(attrs), {
-    include: exports._buildApplicationInclude(attrs)
-  });
+exports._buildApplicationAttrs = function (attrs, include) {
+  // Clone our attrs for extension
+  var retVal = _.clone(attrs);
+
+  // If we have an include parameter
+  if (include) {
+    include.forEach(function handleIncludeItem (includeItem) {
+      // Upcast models to objects
+      if (includeItem.$modelOptions) {
+        includeItem = {model: includeItem};
+      }
+
+      // If the include is an interview, build its matching info
+      if (includeItem.model === Interview) {
+        assert(!includeItem.as, 'Received unexpected "as" key for Interview');
+        retVal.interviews = _.where(interviewMocks, {application_id: attrs.id});
+      // If the include is a reminder , build the matching reminder type
+      } else if (includeItem.model === Reminder) {
+        assert(includeItem.as, 'Missing "as" parameter for Reminder include');
+        if (includeItem.as === 'saved_for_later_reminder') {
+          retVal.saved_for_later_reminder = _.findWhere(reminderMocks,
+            {id: attrs.saved_for_later_reminder_id});
+        } else if (includeItem.as === 'waiting_for_response_reminder') {
+          retVal.waiting_for_response_reminder = _.findWhere(reminderMocks,
+            {id: attrs.waiting_for_response_reminder_id});
+        } else if (includeItem.as === 'received_offer_reminder') {
+          retVal.received_offer_reminder = _.findWhere(reminderMocks,
+            {id: attrs.received_offer_reminder_id});
+        } else {
+          throw new Error('Unrecognized "as" parameter for Reminder: "' + includeItem.as + '"');
+        }
+      // Otherwise, complain and leave
+      } else {
+        throw new Error('Unrecognized model type "' + includeItem.model + '"');
+      }
+    });
+  }
 
   // Return our retVal
+  return retVal;
+};
+function buildApplication(attrs, include) {
+  // Fallback our include parameter temporarily
+  // TODO: Remove include fallback
+  if (!include) {
+    // DEV: We use `_.flatten` and empty arrays to skip non-included data
+    include =  _.flatten([
+      {model: Interview},
+      attrs.saved_for_later_reminder_id ? {model: Reminder, as: 'saved_for_later_reminder'} : [],
+      attrs.waiting_for_response_reminder_id ? {model: Reminder, as: 'waiting_for_response_reminder'} : [],
+      attrs.received_offer_reminder_id ? {model: Reminder, as: 'received_offer_reminder'} : []
+    ]);
+  }
+
+  // Compile our build data based on include
+  var buildData = exports._buildApplicationAttrs(attrs, include);
+
+  // Build and return our application
+  var retVal = Application.build(buildData, {include: include});
   return retVal;
 }
 
@@ -47,9 +83,9 @@ function buildApplication(attrs) {
 exports.getById = function (id) {
   return applicationsById.hasOwnProperty(id) ? buildApplication(applicationsById[id]) : null;
 };
-exports.getByIdOr404 = function (id) {
+exports.getByIdOr404 = function (id, include) {
   // Resolve our application
-  var application = exports.getById(id);
+  var application = exports.getById(id, include);
 
   // If the application doesn't exist, then 404
   if (application === null) {
@@ -59,28 +95,28 @@ exports.getByIdOr404 = function (id) {
   // Otherwise, return our application
   return application;
 };
-exports.getReceivedOfferApplications = function () {
+exports.getReceivedOfferApplications = function (include) {
   return _.where(genericMockData.applications, {
     status: Application.APPLICATION_STATUSES.RECEIVED_OFFER
-  }).map(buildApplication);
+  }).map(function (attrs) { return buildApplication(attrs, include); });
 };
-exports.getUpcomingInterviewApplications = function () {
+exports.getUpcomingInterviewApplications = function (include) {
   return _.where(genericMockData.applications, {
     status: Application.APPLICATION_STATUSES.UPCOMING_INTERVIEW
-  }).map(buildApplication);
+  }).map(function (attrs) { return buildApplication(attrs, include); });
 };
-exports.getWaitingForResponseApplications = function () {
+exports.getWaitingForResponseApplications = function (include) {
   return _.where(genericMockData.applications, {
     status: Application.APPLICATION_STATUSES.WAITING_FOR_RESPONSE
-  }).map(buildApplication);
+  }).map(function (attrs) { return buildApplication(attrs, include); });
 };
-exports.getSavedForLaterApplications = function () {
+exports.getSavedForLaterApplications = function (include) {
   return _.where(genericMockData.applications, {
     status: Application.APPLICATION_STATUSES.SAVED_FOR_LATER
-  }).map(buildApplication);
+  }).map(function (attrs) { return buildApplication(attrs, include); });
 };
-exports.getArchivedApplications = function () {
+exports.getArchivedApplications = function (include) {
   return _.where(genericMockData.applications, {
     status: Application.APPLICATION_STATUSES.ARCHIVED
-  }).map(buildApplication);
+  }).map(function (attrs) { return buildApplication(attrs, include); });
 };
--- server/models/interview-mock-data.js
+++ server/models/interview-mock-data.js
@@ -1,4 +1,5 @@
 // Load in our dependencies
+var assert = require('assert');
 var _ = require('underscore');
 var HttpError = require('http-errors');
 var Application = require('./application');
@@ -16,25 +17,65 @@ genericMockData.interviews.forEach(function saveInterviewById (interview) {
 // Define interview builder
 var applicationMocks = genericMockData.applications;
 var reminderMocks = genericMockData.reminders;
-function buildInterview(attrs) {
-  // Build our interview with its application (and its interviews) and reminders
-  // http://docs.sequelizejs.com/en/v3/docs/associations/#creating-with-associations
-  var applicationAttrs = applicationMockData._buildApplicationAttrs(
-    _.findWhere(applicationMocks, {id: attrs.application_id}));
-  var retVal = Interview.build(_.extend({}, attrs, {
-    application: applicationAttrs,
-    pre_interview_reminder: _.findWhere(reminderMocks, {id: attrs.pre_interview_reminder_id}),
-    post_interview_reminder: _.findWhere(reminderMocks, {id: attrs.post_interview_reminder_id})
-  }), {
-    // TODO: Relocate include calls to controllers
-    include: [
-      {model: Application, include: applicationMockData._buildApplicationInclude(applicationAttrs)},
+function buildInterview(attrs, include) {
+  // Fallback our include parameter temporarily
+  // TODO: Remove include fallback
+  if (!include) {
+    var _applicationAttrs = _.findWhere(applicationMocks, {id: attrs.application_id});
+    include = [
+      {
+        model: Application, include: _.flatten([
+          {model: Interview},
+          _applicationAttrs.saved_for_later_reminder_id ?
+            {model: Reminder, as: 'saved_for_later_reminder'} : [],
+          _applicationAttrs.waiting_for_response_reminder_id ?
+            {model: Reminder, as: 'waiting_for_response_reminder'} : [],
+          _applicationAttrs.received_offer_reminder_id ?
+            {model: Reminder, as: 'received_offer_reminder'} : []
+        ])
+      },
       {model: Reminder, as: 'pre_interview_reminder'},
       {model: Reminder, as: 'post_interview_reminder'}
-    ]
-  });
+    ];
+  }
 
-  // Return our retVal
+  // Compile our build data based on include
+  var buildData = _.clone(attrs);
+  if (include) {
+    include.forEach(function handleIncludeItem (includeItem) {
+      // Upcast models to objects
+      if (includeItem.$modelOptions) {
+        includeItem = {model: includeItem};
+      }
+
+      // If the include is an application, load include data from applicationMockData's resolver
+      if (includeItem.model === Application) {
+        assert(!includeItem.as, 'Received unexpected "as" key for Application');
+        var applicationAttrs = applicationMockData._buildApplicationAttrs(
+          _.findWhere(applicationMocks, {id: attrs.application_id}), includeItem.include);
+        buildData.application = applicationAttrs;
+      // If our include is a reminder, build the matching reminder type
+      } else if (includeItem.model === Reminder) {
+        assert(includeItem.as, 'Missing "as" parameter for Reminder include');
+        if (includeItem.as === 'pre_interview_reminder') {
+          buildData.pre_interview_reminder = _.findWhere(reminderMocks,
+            {id: attrs.pre_interview_reminder_id});
+        } else if (includeItem.as === 'post_interview_reminder') {
+          buildData.post_interview_reminder = _.findWhere(reminderMocks,
+            {id: attrs.post_interview_reminder_id});
+        } else {
+          throw new Error('Unrecognized "as" parameter for Reminder: "' + includeItem.as + '"');
+        }
+      // Otherwise, complain and leave
+      } else {
+        throw new Error('Unrecognized model type "' + includeItem.model + '"');
+      }
+    });
+  }
+
+  // Build and return our interview with its application (and its interviews) and reminders
+  // http://docs.sequelizejs.com/en/v3/docs/associations/#creating-with-associations
+  var retVal = Interview.build(buildData, {include: include});
   return retVal;
 }
 
@@ -42,9 +83,9 @@ function buildInterview(attrs) {
 exports.getById = function (id) {
   return interviewsById.hasOwnProperty(id) ? buildInterview(interviewsById[id]) : null;
 };
-exports.getByIdOr404 = function (id) {
+exports.getByIdOr404 = function (id, include) {
   // Resolve our interview
-  var interview = exports.getById(id);
+  var interview = exports.getById(id, include);
 
   // If the interview doesn't exist, then 404
   if (interview === null) {
